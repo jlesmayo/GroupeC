ea3580dfaa5c10808f9ba40350ce1a0c711b686b
Flavien LEON <flavien.leon@hotmail.fr>
2023-03-03 10:28:23 +0100

    modif pour le group C

--- Azurerm_network.tf
+++ Azurerm_network.tf
@@ -1,11 +1,11 @@
 resource "azurerm_virtual_network" "example" {
-  name                = "Intervenants-vnet" # to change by Groupe ....
-  resource_group_name = data.azurerm_resource_group.GroupeN.name
-  location            = data.azurerm_resource_group.GroupeN.location
+  name                = "Intervenants-vnet_GC" # to change by Groupe ....
+  resource_group_name = data.azurerm_resource_group.rg_51_groupec.name
+  location            = data.azurerm_resource_group.rg_51_groupec.location
   address_space       = ["10.0.0.0/16"] # to change by Groupe ....
 
   tags = {
-    environment = "Ecole_CAP_Azure"
+    environment = "Ecole_CAP_Azure_GC"
     session     = "1"
     groupe      = "0"
   }
--- Azurerm_network_interface.tf
+++ Azurerm_network_interface.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_interface" "example" {
   name                = "${var.vm_name_pfx}-nic"
-  location            = data.azurerm_resource_group.GroupeN.location
-  resource_group_name = data.azurerm_resource_group.GroupeN.name
+  location            = data.azurerm_resource_group.rg_51_groupec.location
+  resource_group_name = data.azurerm_resource_group.rg_51_groupec.name
 
   ip_configuration {
     name                          = "internal"
@@ -10,7 +10,7 @@ resource "azurerm_network_interface" "example" {
   }
 
   tags = {
-    environment = "Ecole_CAP_Azure"
+    environment = "Ecole_CAP_Azure-GC"
     session     = "1"
     groupe      = "0"
   }
